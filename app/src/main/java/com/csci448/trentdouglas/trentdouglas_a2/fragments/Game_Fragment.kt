85da76a4fefdec22b6db56304a720164573ba22c
trentdouglas99 <trentdouglas@mymail.mines.edu>
2021-03-16 11:02:14 -0600

    smart joe activated

--- app/src/main/java/com/csci448/trentdouglas/trentdouglas_a2/fragments/Game_Fragment.kt
+++ app/src/main/java/com/csci448/trentdouglas/trentdouglas_a2/fragments/Game_Fragment.kt
@@ -17,7 +17,7 @@ import com.csci448.trentdouglas.trentdouglas_a2.databinding.GameFragmentBinding
 
 class Game_Fragment: Fragment() {
     private var _binding: GameFragmentBinding? = null
-
+    private var move = 1
     var used_list = mutableListOf(false, false, false, false, false, false, false, false, false)
 
     var player_chose = mutableListOf<Int>()
@@ -25,6 +25,7 @@ class Game_Fragment: Fragment() {
     var player_wins = false
     var computer_wins = false
     var game_over = false
+
     private lateinit var game_Fragment_View_Model: GameFragmentViewModel
 
     // This property is only valid between onCreateView and onDestroyView
@@ -35,10 +36,11 @@ class Game_Fragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        move = 1
 
         var sharedPref = getDefaultSharedPreferences(context)
         var darkMode = sharedPref.getBoolean("dark_mode", false)
-
+        var smartJoe = sharedPref.getBoolean("smart_joe", false)
 
 
         _binding = GameFragmentBinding.inflate(inflater, container, false)
@@ -46,6 +48,8 @@ class Game_Fragment: Fragment() {
         if(darkMode){
             binding.background.setBackgroundColor(resources.getColor(R.color.black))
         }
+
+
 
         used_list = mutableListOf<Boolean>(false, false, false, false, false, false, false, false, false)
         binding.one.setOnClickListener {
@@ -55,7 +59,8 @@ class Game_Fragment: Fragment() {
                     used_list[0] = true
                     player_chose.add(0)
                     check_for_win()
-                    computer_play()
+                    if(smartJoe) smart_joe_play()
+                    else computer_play()
 
                 }
             }
@@ -67,7 +72,8 @@ class Game_Fragment: Fragment() {
                     used_list[1] = true
                     player_chose.add(1)
                     check_for_win()
-                    computer_play()
+                    if(smartJoe) smart_joe_play()
+                    else computer_play()
 
                 }
             }
@@ -80,7 +86,8 @@ class Game_Fragment: Fragment() {
                     used_list[2] = true
                     player_chose.add(2)
                     check_for_win()
-                    computer_play()
+                    if(smartJoe) smart_joe_play()
+                    else computer_play()
 
                 }
             }
@@ -93,7 +100,8 @@ class Game_Fragment: Fragment() {
                     used_list[3] = true
                     player_chose.add(3)
                     check_for_win()
-                    computer_play()
+                    if(smartJoe) smart_joe_play()
+                    else computer_play()
 
                 }
             }
@@ -106,7 +114,8 @@ class Game_Fragment: Fragment() {
                     used_list[4] = true
                     player_chose.add(4)
                     check_for_win()
-                    computer_play()
+                    if(smartJoe) smart_joe_play()
+                    else computer_play()
 
                 }
             }
@@ -119,7 +128,8 @@ class Game_Fragment: Fragment() {
                     used_list[5] = true
                     player_chose.add(5)
                     check_for_win()
-                    computer_play()
+                    if(smartJoe) smart_joe_play()
+                    else computer_play()
 
                 }
             }
@@ -132,7 +142,8 @@ class Game_Fragment: Fragment() {
                     used_list[6] = true
                     player_chose.add(6)
                     check_for_win()
-                    computer_play()
+                    if(smartJoe) smart_joe_play()
+                    else computer_play()
                 }
             }
 
@@ -144,7 +155,8 @@ class Game_Fragment: Fragment() {
                     used_list[7] = true
                     player_chose.add(7)
                     check_for_win()
-                    computer_play()
+                    if(smartJoe) smart_joe_play()
+                    else computer_play()
 
                 }
             }
@@ -157,7 +169,8 @@ class Game_Fragment: Fragment() {
                     used_list[8] = true
                     player_chose.add(8)
                     check_for_win()
-                    computer_play()
+                    if(smartJoe) smart_joe_play()
+                    else computer_play()
 
                 }
             }
@@ -226,13 +239,159 @@ class Game_Fragment: Fragment() {
             binding.playAgain.visibility = View.VISIBLE
         }
         if(computer_wins){
-            Toast.makeText(requireContext(), "Computer Wins!", Toast.LENGTH_SHORT).show()
+            var sharedPref = getDefaultSharedPreferences(context)
+            var smartJoe = sharedPref.getBoolean("smart_joe", false)
+
+            if(smartJoe) Toast.makeText(requireContext(), "Smart Joe Wins!", Toast.LENGTH_SHORT).show()
+            else Toast.makeText(requireContext(), "Average Joe Wins!", Toast.LENGTH_SHORT).show()
+
             val game = Game()
             game_over = true
-            game.winner = "Computer"
+
+            if(smartJoe)game.winner = "Smart Joe"
+            else game.winner = "Average Joe"
             game_Fragment_View_Model.addGame(game)
             binding.back.visibility = View.VISIBLE
             binding.playAgain.visibility = View.VISIBLE
+        }
+
+    }
+
+    fun smart_joe_play(){
+        if(!game_over) {
+            var count = 0
+            var used_count = 0
+            while (count < 9) {
+                if (used_list[count]) {
+                    used_count++
+                }
+                count++
+            }
+            if (used_count == 9) return
+
+            var choice = 6
+
+
+            if((player_chose.contains(3) || player_chose.contains(7)) && move == 2) choice = 8
+            else if(player_chose.contains(3) && player_chose.contains(7) && move == 3) choice = 2
+            else if(computer_chose.contains(6) && computer_chose.contains(8) && computer_chose.contains(2) && !player_chose.contains(4) && move == 4) choice = 4
+            else if(computer_chose.contains(6) && computer_chose.contains(8) && computer_chose.contains(2) && !player_chose.contains(5) && move == 4) choice = 5
+
+            else if(player_chose.contains(0) && move == 2) choice = 8
+            else if(player_chose.contains(0) && player_chose.contains(7) && move == 3) choice = 2
+
+
+
+            else if(player_chose.contains(5) && move == 2) choice = 8
+            else if(player_chose.contains(5) && player_chose.contains(7) && move == 3) choice = 0
+            else if(computer_chose.contains(0) && computer_chose.contains(8) && computer_chose.contains(2) && !player_chose.contains(3) && move == 4) choice = 3
+            else if(computer_chose.contains(0) && computer_chose.contains(8) && computer_chose.contains(6) && !player_chose.contains(4) && move == 4) choice = 4
+
+            else if(player_chose.contains(2) && move == 2) choice = 8
+            else if(player_chose.contains(2) && player_chose.contains(7) && move == 3) choice = 0
+            else if(computer_chose.contains(0) && computer_chose.contains(8) && computer_chose.contains(2) && !player_chose.contains(3) && move == 4) choice = 3
+            else if(computer_chose.contains(0) && computer_chose.contains(8) && computer_chose.contains(6) && !player_chose.contains(4) && move == 4) choice = 4
+
+
+            if(player_chose.contains(0) && player_chose.contains(1) && !computer_chose.contains(2)) choice = 2
+            if(player_chose.contains(0) && player_chose.contains(2) && !computer_chose.contains(1)) choice = 1
+            if(player_chose.contains(1) && player_chose.contains(2) && !computer_chose.contains(0)) choice = 0
+
+            if(player_chose.contains(4) && player_chose.contains(5) && !computer_chose.contains(3)) choice = 3
+            if(player_chose.contains(3) && player_chose.contains(5) && !computer_chose.contains(4)) choice = 4
+            if(player_chose.contains(3) && player_chose.contains(4) && !computer_chose.contains(5)) choice = 5
+
+            if(player_chose.contains(7) && player_chose.contains(8) && !computer_chose.contains(6)) choice = 6
+            if(player_chose.contains(6) && player_chose.contains(8) && !computer_chose.contains(7)) choice = 7
+            if(player_chose.contains(6) && player_chose.contains(7) && !computer_chose.contains(8)) choice = 8
+
+            if(player_chose.contains(3) && player_chose.contains(6) && !computer_chose.contains(0)) choice = 0
+            if(player_chose.contains(0) && player_chose.contains(6) && !computer_chose.contains(3)) choice = 3
+            if(player_chose.contains(0) && player_chose.contains(3) && !computer_chose.contains(6)) choice = 6
+
+            if(player_chose.contains(4) && player_chose.contains(7) && !computer_chose.contains(1)) choice = 1
+            if(player_chose.contains(1) && player_chose.contains(7) && !computer_chose.contains(4)) choice = 4
+            if(player_chose.contains(1) && player_chose.contains(4) && !computer_chose.contains(7)) choice = 7
+
+            if(player_chose.contains(5) && player_chose.contains(8) && !computer_chose.contains(2)) choice = 2
+            if(player_chose.contains(2) && player_chose.contains(8) && !computer_chose.contains(5)) choice = 5
+            if(player_chose.contains(2) && player_chose.contains(5) && !computer_chose.contains(8)) choice = 8
+
+            if(player_chose.contains(4) && player_chose.contains(8) && !computer_chose.contains(0)) choice = 0
+            if(player_chose.contains(0) && player_chose.contains(8) && !computer_chose.contains(4)) choice = 4
+            if(player_chose.contains(0) && player_chose.contains(4) && !computer_chose.contains(8)) choice = 8
+
+            if(player_chose.contains(4) && player_chose.contains(2) && !computer_chose.contains(6)) choice = 6
+            if(player_chose.contains(6) && player_chose.contains(2) && !computer_chose.contains(4)) choice = 4
+            if(player_chose.contains(6) && player_chose.contains(4) && !computer_chose.contains(2)) choice = 2
+
+            if(computer_chose.contains(6) && computer_chose.contains(8) && !player_chose.contains(7)) choice = 7
+            if(computer_chose.contains(6) && computer_chose.contains(0) && !player_chose.contains(3)) choice = 3
+            if(computer_chose.contains(6) && computer_chose.contains(2) && !player_chose.contains(4)) choice = 4
+            if(computer_chose.contains(6) && computer_chose.contains(3) && !player_chose.contains(0)) choice = 0
+            if(computer_chose.contains(6) && computer_chose.contains(4) && !player_chose.contains(2)) choice = 2
+            if(computer_chose.contains(6) && computer_chose.contains(7) && !player_chose.contains(8)) choice = 8
+            if(computer_chose.contains(2) && computer_chose.contains(8) && !player_chose.contains(5)) choice = 5
+
+
+            else if(choice == 6 && move != 1){
+                choice = (0..8).random()
+                while (used_list[choice]) {
+                    choice = (0..8).random()
+                }
+            }
+
+            move++
+
+
+
+
+            if (choice == 0) {
+                binding.one.background = resources.getDrawable(R.drawable.oh)
+                used_list[0] = true
+                computer_chose.add(0)
+            }
+            if (choice == 1) {
+                binding.two.background = resources.getDrawable(R.drawable.oh)
+                used_list[1] = true
+                computer_chose.add(1)
+            }
+            if (choice == 2) {
+                binding.three.background = resources.getDrawable(R.drawable.oh)
+                used_list[2] = true
+                computer_chose.add(2)
+            }
+            if (choice == 3) {
+                binding.four.background = resources.getDrawable(R.drawable.oh)
+                used_list[3] = true
+                computer_chose.add(3)
+            }
+            if (choice == 4) {
+                binding.five.background = resources.getDrawable(R.drawable.oh)
+                used_list[4] = true
+                computer_chose.add(4)
+            }
+            if (choice == 5) {
+                binding.six.background = resources.getDrawable(R.drawable.oh)
+                used_list[5] = true
+                computer_chose.add(5)
+            }
+            if (choice == 6) {
+                binding.seven.background = resources.getDrawable(R.drawable.oh)
+                used_list[6] = true
+                computer_chose.add(6)
+            }
+            if (choice == 7) {
+                binding.eight.background = resources.getDrawable(R.drawable.oh)
+                used_list[7] = true
+                computer_chose.add(7)
+            }
+            if (choice == 8) {
+                binding.nine.background = resources.getDrawable(R.drawable.oh)
+                used_list[8] = true
+                computer_chose.add(8)
+            }
+            check_for_win()
         }
 
     }
@@ -344,11 +503,14 @@ class Game_Fragment: Fragment() {
 
 
     override fun onResume(){
+        var sharedPref = getDefaultSharedPreferences(context)
+        var smartJoe = sharedPref.getBoolean("smart_joe", false)
         Log.d(LOG_TAG, "onResume()")
         game_over = game_Fragment_View_Model.gameOver
         used_list = game_Fragment_View_Model.games
         player_chose = game_Fragment_View_Model.playerChose
         computer_chose = game_Fragment_View_Model.computerChose
+        if(smartJoe) smart_joe_play()
 
         if(game_over){
             binding.back.visibility = View.VISIBLE
